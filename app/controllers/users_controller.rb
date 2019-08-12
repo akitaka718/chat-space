33d44bff158709d63d228cdb749f00ec7100e33a
akitaka718 <rsp81183@gmail.com>
2019-08-12 20:41:57 +0900

    update-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-      params.require(:user).permit(:name,:email)
+      params.require(:user).permit(:nickname,:email)
   end    
 end
 
