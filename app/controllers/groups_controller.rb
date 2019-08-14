99ba7ae31e40da5bf2853679ea6106d5f1021baf
akitaka718 <rsp81183@gmail.com>
2019-08-14 11:56:47 +0900

    confrikuto

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,29 +4,25 @@ class GroupsController < ApplicationController
   end
 
   def create
-<<<<<<< Updated upstream
-=======
+
     @group = Group.new(group_params)
     if @group.save
        redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
-     
->>>>>>> Stashed changes
   end
 
   def edit
   end
 
-<<<<<<< Updated upstream
+
   def update
   end
-  
-=======
+
   private
   def group_params
     params.require(:group).permit( :name,{:user_ids => [] })
   end
->>>>>>> Stashed changes
+
 end
