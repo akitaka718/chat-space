4616ea66d7d3268e95185d73ccaa8c09b5536c94
akitaka718 <rsp81183@gmail.com>
2019-08-14 16:21:06 +0900

    add-group-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-   
+  before_action :set_group, only: [:edit,:update] 
   def new
     @group=Group.new
     @group.users << current_user
@@ -8,18 +8,19 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-       redirect_to root_path, notice: 'グループを作成しました'
+       redirect_to group_messages_path(@group), notice: 'グループを作成しました'
     else
       render :new
     end
   end
 
-  
-  def update
-  end
-
   private
+  
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
-  end  
+  end
+
+  def set_group
+    @group=Group.find(params[:id])
+  end 
 end
