680cf08256e09bd88509b7be6394aa2c6936a1ba
akitaka718 <rsp81183@gmail.com>
2019-08-14 10:02:12 +0900

    add-group_loop

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,32 @@
 class GroupsController < ApplicationController
+   
   def new
   end
 
   def create
+<<<<<<< Updated upstream
+=======
+    @group = Group.new(group_params)
+    if @group.save
+       redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+     
+>>>>>>> Stashed changes
   end
 
   def edit
   end
 
+<<<<<<< Updated upstream
   def update
   end
   
+=======
+  private
+  def group_params
+    params.require(:group).permit( :name,{:user_ids => [] })
+  end
+>>>>>>> Stashed changes
 end
