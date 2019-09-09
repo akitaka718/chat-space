53f49fa3a3abbcee90e456d6cf96654054f86d19
akitaka718 <rsp81183@gmail.com>
2019-09-09 20:46:05 +0900

    Update index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,10 +3,20 @@ $(function(){
     var html=`
     <div class="chat-group-user clearfix">
       <p class="chat-group-user__name">${user.name}</p>
-      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
     </div>`
     return html
   }
+  
+  function noMatchUser(noUser){
+    var html=`
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${noUser}</p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+    </div>`
+
+  }
+
   $('#user-search-field').on('keyup',function(){
     var input=$('#user-search-field').val();
     $.ajax({
@@ -15,13 +25,20 @@ $(function(){
       data: {name: input},
       dataType: 'json'
     })
-    .done(function(data){
-      html=biuldHtml(data)
-      console.log(html);
-      $('#user-search-result').append(html);
+    .done(function(datas){
+      if (datas.length!==0){
+        datas.forEach(function(data){
+          html=biuldHtml(data)
+          console.log(data);
+          console.log(html);
+          $('#user-search-result').append(html);
+        });
+      }   
     })
+    
     .fail(function(){
-     alert('エラーが発生しました');
+     alert('ユーザー検索に失敗しました');
     })
+  
   })
 });
