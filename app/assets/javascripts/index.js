67b36b262033f4213bffbeacc80c011530287437
akitaka718 <rsp81183@gmail.com>
2019-09-09 19:51:52 +0900

    Update index.js
    ユーザー名を自動表示される様に修正

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,7 +2,7 @@ $(function(){
   function biuldHtml(index){
     var html=`
     <div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">ユーザー名</p>
+      <p class="chat-group-user__name">${user.name}</p>
       <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
     </div>`
     return html
