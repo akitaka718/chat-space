ce6660e1c8a3c723af439e3a9a516c744b2cec7c
akitaka718 <rsp81183@gmail.com>
2019-09-09 19:28:58 +0900

    Update index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,17 +1,27 @@
 $(function(){
+  function biuldHtml(index){
+    var html=`
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">ユーザー名</p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+    </div>`
+    return html
+  }
   $('#user-search-field').on('keyup',function(){
     var input=$('#user-search-field').val();
     $.ajax({
       type: 'GET',
       url: '/users',
-      data: {keyword: input},
+      data: {name: input},
       dataType: 'json'
     })
     .done(function(data){
-      console.log('成功');
+      html=biuldHtml(data)
+      console.log(html);
+      $('#user-search-result').append(html);
     })
     .fail(function(){
-      alert('失敗');
+    
     })
   })
 });
