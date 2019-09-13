d4190ae969aedb247346b37f21e74f2c7291e89e
akitaka718 <rsp81183@gmail.com>
2019-09-13 21:42:20 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,5 @@
 $(function(){
-  
+  //テンプレートリテラル記法でHTML作成 
   function buildHtml(message){
     var image = message.image? `<img src=${message.image}>`:"";
     var html=`<div class="message">
@@ -19,6 +19,7 @@ $(function(){
     return html
   }
   
+  //非同期通信
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formdata = new FormData(this);
@@ -43,4 +44,26 @@ $(function(){
       $('.form__submit').prop("disabled",false);
     })
   })
+  
+  //自動更新
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用して、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data('message-id');
+    $.ajax({
+      url: '/api/messages',
+      //ターミナルでroutesのVerb項目を確認。 api/messages#indexはGETとなっているのでGETを設定
+      type: 'GET',
+      //最新のメッセージを値にしてリクエストする
+      data: {id: last_message_id},
+      //データ要求方式
+      dataType: 'json'
+    })
+
+    .done(function(messages){
+      console.log(messages);
+    })
+    .fail(function(){
+      console.log('自動更新に失敗しました');
+    })
+  }
 });
