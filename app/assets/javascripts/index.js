412e21fd6f660aaff975e8f11e9eb9a05f108e82
akitaka718 <rsp81183@gmail.com>
2019-09-13 13:51:06 +0900

    update-index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,9 +1,9 @@
 $(function(){
   function biuldHtml(user){
     var html=`
-    <div class="chat-group-user clearfix">
+    <div class="chat-group-user clearfix">
       <p class="chat-group-user__name">${user.name}</p>
-      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="user.id" data-user-name="user.name">追加</div>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
     </div>`
     return html
   }
@@ -16,6 +16,17 @@ $(function(){
     return html
   }
 
+  function addBtnClick(user,userId){
+    const addGroupMenber=$('.user-group-add-result')
+    var html=`
+    <div class='chat-group-user'>
+      <input name='group[user_ids][]' type='hidden' value=${userId}>
+      <p class='chat-group-user__name'>${user}</p>
+      <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+    </div>`
+    addGroupMenber.append(html);
+  }
+  
   $('#user-search-field').on('keyup',function(){
     var input=$('#user-search-field').val();
     $.ajax({
@@ -26,6 +37,7 @@ $(function(){
     })
     .done(function(datas){
       $('#user-search-result').empty();
+      console.log(input);
       if (datas.length!==0){
         datas.forEach(function(data){
           html=biuldHtml(data)
@@ -35,12 +47,22 @@ $(function(){
       else{
         html = noMatchUser('一致するユーザーが存在しません');
         $('#user-search-result').append(html);
-      }   
+      }
     })
-    
     .fail(function(){
-     alert('ユーザー検索に失敗しました');
+      alert('ユーザー検索に失敗しました');
     })
-  
   })
+   
+  $(document).on('click','.user-search-add.chat-group-user__btn.chat-group-user__btn--add',function(){
+    var userName=$(this).attr('data-user-name');//data-user-name属性の属性値を取得
+    var userId=$(this).attr('data-user-id');//data-user-id属性の属性値を取得
+    addBtnClick(userName,userId)
+    $(this).parent().remove();//$(document).on()で指定した要素(親要素を含めて)を削除  
+  })
+
+  $(document).on('click','.user-search-remove.chat-group-user__btn.chat-group-user__btn--remove.js-remove-btn',function(){
+    $(this).parent().remove();
+  })
+
 });
