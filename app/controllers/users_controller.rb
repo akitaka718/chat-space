510902de50c6e7850b0bb9d6950a074c3d724410
akitaka718 <rsp81183@gmail.com>
2019-09-13 15:45:41 +0900

    Update users_controller.rb
    users_controller.rbを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-     @users=User.where('nickname LIKE(?)',"%#{params[:name]}%").where.not(id: current_user.id)
+     @users=User.where('nickname LIKE(?) and id !=?',"%#{params[:name]}%", current_user)
      respond_to do |format| 
        format.html
        format.json
