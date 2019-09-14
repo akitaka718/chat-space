373e89bc066f72cf067462ee9903c78d90f42a79
akitaka718 <rsp81183@gmail.com>
2019-09-14 20:41:34 +0900

    updat

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
   //テンプレートリテラル記法でHTML作成 
   function buildHtml(message){
     var image = message.image? `<img src=${message.image}>`:"";
-    var html=`<div class="message">
+    var html=`<div class="message" data-message-id='${message.id}'>
                <div class="message-box">
                 <div class="message-user">
                  ${message.name}
@@ -46,27 +46,7 @@ $(function(){
   })
   
   //---自動更新---
-  var buildMessageHTML = function(message) {
-    if (message.body || message.img.url) {
-      var html=`
-      <div class = "message" data-message_id = '${message.id}'>
-        <div class ="message-box">
-           <div class="message-user">
-             ${message.nickname}
-           </div>
-           <div class="message-date">
-             ${message.created-at}
-           </div>
-        </div>   
-        <div class="message-body">
-          ${message.body}
-        </div>
-        ${image} 
-      </div>`
-    }      
-    return html
-  };
-  
+
   var reloadMessages = function() {
     //カスタムデータ属性を利用して、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.message:last').data('message-id');
@@ -95,12 +75,14 @@ $(function(){
     })
 
     .done(function(messages){
-      messages.foreach(function(message){
+      var insertHtml ="";
+      messages.forEach(function(message){
         //メッセージが入ったHTMLを取得
-        insertHtml = buildMessageHTML(message);
+        insertHtml = buildHtml(message);
         //取得したHTMLを表示
         $('.messages').append(insertHtml);
       });
+  
     })
     .fail(function(){
       console.log('自動更新に失敗しました');
--- app/views/api/messages/index.json.jbuilder
+++ app/views/api/messages/index.json.jbuilder
@@ -3,6 +3,6 @@ json.array! @messages do |message|
   json.image message.image.url
   json.created_at message.created_at
   json.id message.id 
-  json.nickname message.user.nickname
+  json.name message.user.nickname
 end
 #更新される間に複数メッセージが投稿される可能性があるのので、配列で設定して値を表示させる
