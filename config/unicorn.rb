5fdf89bcce5ee5f74eb4796ea93063eb9105d8ae
akitaka718 <rsp81183@gmail.com>
2019-09-19 13:45:32 +0900

    add-autoDeploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,37 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に格納しておく
-app_path = File.expand_path('../../', __FILE__)
+#app_path = File.expand_path('../../', __FILE__)
+#↓自動デプロイ時のでディレクトリ指定
+app_path = File.expand_path('../../../', __FILE__)
 
 #アプリケーションサーバーアプリケーションサーバーの性能を決定する
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+#working_directory app_path
+#↓自動デプロイ時の設定
+working_directory "#{app_path}/current"
+
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
+#pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+#listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+#stderr_path "#{app_path}/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+#stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+
+# それぞれ、sharedの中を参照する様に変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicron.stdout.log"
+
+
 
 #Railsアプリケーションの応答を待つ上限上限時間を設定
 timeout 60
